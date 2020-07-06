5712bbd7be199898caf943507d7e7c3c70c61f0e
Daniel Qi <drqi@gmail.com>
2020-07-05 17:44:52 -0700

    Swapped to jQuery

---
+++ jqueryTasks.js
@@ -0,0 +1,173 @@
+
+var storage = window.localStorage;
+
+$(function() {
+	if(storage.getItem("tasks") !== null)
+	{
+		var taskString, task, completed;
+
+		var previousList = JSON.parse(storage.getItem("tasks"));
+
+		var tc = 0; 
+
+		AddTaskToList(previousList, tc);
+
+	}
+	else
+	{
+		console.log("nothing to load");
+	}
+});
+
+$(function() {
+	$('li').hover(
+		function() {
+			$(this).addClass('selected');
+		},
+		function() {
+			$(this).removeClass('selected');
+		});
+
+	$('li .delete').on('click', function() {
+		$(this).parent().slideUp(300, DeleteTask())
+	});
+
+	$('#createnewtask').on('click', function() {
+		$(CreateNewTaskObject()).slideDown(300);
+	});
+
+	$('#clearall').on('click', function() {
+		var rows = checklist.querySelectorAll("li");
+		HideAndClearTasks(rows, rows.length - 1);
+	});
+});
+
+function HideAndClearTasks(rows, index)
+{
+	setTimeout(function() {
+		var row = rows[index];
+		$(row).slideUp(200, function() {
+			if(index < 5)
+			{
+				var textinput = row.querySelector("input[type='text']")
+				textinput.value = "";
+				SaveTextInput.call(textinput);
+			}
+			else
+			{
+				$(this).remove();
+			}
+		});
+
+		if(index > 0)
+			HideAndClearTasks(rows, --index);
+		else
+			ShowTasks(rows, index);
+	}, 100);
+}
+
+function ShowTasks(rows, index)
+{
+	setTimeout(function() {
+		var row = rows[index];
+		$(row).slideDown(200);
+		if(index < rows.length)
+		{
+			ShowTasks(rows, ++index);
+		}
+	}, 100);
+}
+
+
+function DeleteTask()
+{
+	$(this).parent().slideUp(300, function() {
+			$(this).remove();
+		});
+
+}
+
+function AddTaskToList(previousList, tc)
+{
+	setTimeout(function() {
+
+		if(tc < previousList.length)
+		{
+			var newTask = CreateNewTaskObject.call();
+
+			taskString = previousList[previousList.length - 1 - tc].split(":");
+			task = taskString[0];
+			completed = taskString[1];
+
+			var text = newTask.querySelector("input[type='text']");
+			text.value = task;
+			SaveTextInput.call(text);
+
+			if(parseInt(completed, 10) === 1)
+			{
+				CrossOffTask.call(text.nextElementSibling);
+			}
+
+			$(newTask).slideDown(300);
+		}
+		else if(tc < 5)
+		{
+			var newTask = CreateNewTaskObject.call();
+			$(newTask).slideDown(300);
+		}
+
+		if(tc < previousList.length || tc < 5)
+			AddTaskToList(previousList, ++tc);
+
+	}, 100);
+}
+
+function CreateNewTaskObject()
+{
+	var newTask = document.createElement("li");
+	newTask.classList.add("row");
+
+	var newCB = document.createElement("input");
+	newCB.setAttribute('type', 'button');
+	newCB.classList.add("complete");
+	newCB.value = "Complete";
+
+	var newSpan = document.createElement("span");
+
+	var newInput = document.createElement("input");
+	newInput.classList.add("empty");
+	newInput.setAttribute('type', 'text');
+
+	var newDelButton = document.createElement("input");
+	newDelButton.setAttribute('type', 'button');
+	newDelButton.classList.add("delete");
+	newDelButton.value = "Delete";
+
+	newCB.addEventListener("click", CrossOffTask);
+	newSpan.addEventListener("click", EditItemFromSpan);
+	newInput.addEventListener("click", EditItem);
+	newInput.addEventListener("blur", SaveTextInput);
+	newInput.addEventListener("keydown", InputKeyPress);
+	newInput.addEventListener("input", UpdateInputSize);
+
+
+	newTask.appendChild(newSpan);
+	newTask.appendChild(newInput);
+	newTask.appendChild(newCB);
+	newTask.appendChild(newDelButton);
+	$(newTask).hover(
+		function() {
+			$(this).addClass('selected');
+		},
+		function() {
+			$(this).removeClass('selected');
+		});
+		
+	$(newDelButton).on('click', DeleteTask);
+
+	$(newTask).toggle();
+
+	checklist.appendChild(newTask);
+
+	return newTask;
+}
--- tasks.js
+++ tasks.js
@@ -1,67 +1,23 @@
 
-var storage = window.localStorage;
 
 var checklist = document.getElementById("checklist");
-var body = document.querySelector("body");
-
-var rows = [].slice.call(checklist.querySelectorAll("li"));
-console.log(rows.length);
-
-var cb = [].slice.call(checklist.querySelectorAll("input[type='button'].complete"));
-var spans = [].slice.call(checklist.querySelectorAll("span"));
-var textInputs = [].slice.call(checklist.querySelectorAll("input[type='text']"));
-var delButtons = [].slice.call(checklist.querySelectorAll("input[type='button'].delete"));
 
 var date = document.getElementById("date");
 var time = document.getElementById("time");
 
 var fs = document.getElementById("fakespan");
 
-var clear = document.getElementById("clearall");
-var add = document.getElementById("addtask");
-
-
-clear.addEventListener("click", ClearTasks);
-add.addEventListener("click", AddTask);
-
-document.addEventListener('click', DeselectTask);
 
 window.addEventListener("beforeunload", SaveTasklist);
-
-ReadSavedTaskList();
-
 
 var today = new Date();
 
 date.innerText = "Today is " + today.toLocaleString('default', {month: 'long'}) + " " + today.getDate() + ", " + today.getFullYear();
 
+UpdateTime();
 setInterval(UpdateTime, 500)
 
-for(var i = 0; i < cb.length; ++i)
-{
-	cb[i].addEventListener("click", CrossOffTask);
-	spans[i].addEventListener("click", EditItemFromSpan);
-	textInputs[i].addEventListener("click", EditItem);
-	textInputs[i].addEventListener("blur", SaveTextInput);
-	textInputs[i].addEventListener("keydown", InputKeyPress);
-	textInputs[i].addEventListener("input", UpdateInputSize)
-	rows[i].addEventListener("click",SelectTask);
-	delButtons[i].addEventListener("click", DeleteTask);
-}
 
-
-
-
-
-/* 
-Todo: 
-	get input width to increase with text/decrease with text
-	clear all button 
-	do smth when all tasks are completed
-	start with only one empty task
-	button to add another empty task 
-	button to delete a task 
-*/
 
 
 
@@ -69,108 +25,8 @@ Todo:
 
 //Below are all functions
 
-function DeleteTask()
-{
-	var delNode = checklist.removeChild(this.parentNode);
-
-	rows = [].slice.call(checklist.querySelectorAll("li"));
-
-	cb = [].slice.call(checklist.querySelectorAll("input[type='button'].complete"));
-	spans = [].slice.call(checklist.querySelectorAll("span"));
-	textInputs = [].slice.call(checklist.querySelectorAll("input[type='text']"));
-	delButtons = [].slice.call(checklist.querySelectorAll("input[type='button'].delete"));
-}
-
-function DeselectTask(event)
-{
-	for(var i = 0; i < rows.length; ++i)
-	{
-		if(!rows[i].contains(event.target) && rows[i].classList.contains("selected"))
-		{
-			rows[i].classList.remove("selected");
-		}
-	}
-}
-
-function SelectTask(event)
-{
-
-	if(!this.classList.contains("selected"))
-	{
-		console.log("parent selected");
-		this.classList.add("selected");
-	}
-}
 
 
-function ClearTasks()
-{
-	for(var i = rows.length - 1; i >= 0; --i)
-	{
-		if(i < 5)
-		{
-			textInputs[i].value = "";
-			SaveTextInput.call(textInputs[i]);
-		}
-		else
-		{
-			checklist.removeChild(rows[i]);
-			rows.pop();
-			cb.pop();
-			spans.pop();
-			textInputs.pop();
-			delButtons.pop();
-		}
-	}
-}
-
-function AddTask()
-{
-	var newTask = document.createElement("li");
-	newTask.classList.add("row");
-	newTask.addEventListener("click",SelectTask);
-
-	var newCB = document.createElement("input");
-	newCB.setAttribute('type', 'button');
-	newCB.classList.add("complete");
-	newCB.value = "Complete";
-
-	var newSpan = document.createElement("span");
-
-	var newInput = document.createElement("input");
-	newInput.classList.add("empty");
-	newInput.setAttribute('type', 'text');
-
-	var newDelButton = document.createElement("input");
-	newDelButton.setAttribute('type', 'button');
-	newDelButton.classList.add("delete");
-	newDelButton.value = "Delete";
-
-
-	newCB.addEventListener("click", CrossOffTask);
-	newSpan.addEventListener("click", EditItemFromSpan);
-	newInput.addEventListener("click", EditItem);
-	newInput.addEventListener("blur", SaveTextInput);
-	newInput.addEventListener("keydown", InputKeyPress);
-	newInput.addEventListener("input", UpdateInputSize);
-	newDelButton.addEventListener("click", DeleteTask);
-
-
-	rows.push(newTask);
-	spans.push(newSpan);
-	cb.push(newCB);
-	textInputs.push(newInput);
-	delButtons.push(newDelButton);
-
-
-	newTask.appendChild(newSpan);
-	newTask.appendChild(newInput);
-	newTask.appendChild(newCB);
-	newTask.appendChild(newDelButton);
-
-	checklist.appendChild(newTask);
-
-}
 
 function UpdateTime()
 {
@@ -209,24 +65,15 @@ function CrossOffTask()
 
 function EditItem(event)
 {	
-	if(this.parentNode.classList.contains("selected"))
-	{
-		console.log("selecting child");
-		this.parentNode.classList.add("edit");
-	 	this.value = this.previousElementSibling.innerText;	
-	 	this.focus();
-	 	this.setSelectionRange(0, this.value.length);
-	}
-	else
-	{
-		this.blur();
-	}
+	this.parentNode.classList.add("edit");
+	this.value = this.previousElementSibling.innerText;	
+	this.focus();
+	this.setSelectionRange(0, this.value.length);
 }
 
 function EditItemFromSpan(event)
 {
-	if(this.parentNode.classList.contains("selected"))
-		EditItem.call(this.nextElementSibling, event);
+	EditItem.call(this.nextElementSibling);
 }
 
 function SaveTextInput()
@@ -249,9 +96,28 @@ function SaveTextInput()
 	this.parentNode.classList.remove("edit");
 }
 
+
+function SaveTasklist()
+{
+	var taskArray = [];
+	var taskString = "";
+
+	var spans = [].slice.call(checklist.querySelectorAll("span"));
+
+	for(var i = spans.length - 1; i >= 0; --i)
+	{
+		if(spans[i].innerText.length == 0)
+			continue;
+		taskString = spans[i].innerText + ":" + (spans[i].classList.contains("completed") ? 1 : 0)
+		taskArray.push(taskString);
+	}
+
+	storage.setItem("tasks", JSON.stringify(taskArray));
+
+}
+
 function UpdateInputSize()
 {
-	fs.innerText = this.value;
 	this.style.width = Math.max(100, fs.clientWidth) + "px";
 }
 
@@ -268,57 +134,4 @@ function InputKeyPress(event)
 
 }
 
-function SaveTasklist()
-{
-	var taskArray = [];
-	var taskString = "";
-	for(var i = rows.length - 1; i >= 0; --i)
-	{
-		if(spans[i].innerText.length == 0)
-			continue;
-		taskString = spans[i].innerText + ":" + (spans[i].classList.contains("completed") ? 1 : 0)
-		taskArray.push(taskString);
-	}
 
-	storage.setItem("tasks", JSON.stringify(taskArray));
-
-}
-
-function ReadSavedTaskList()
-{
-	if(storage.getItem("tasks") !== null)
-	{
-		var taskString, task, completed;
-
-		var previousList = JSON.parse(storage.getItem("tasks"));
-
-		if(previousList.length > 5)
-		{
-			for(var i = 0; i < previousList.length - 5; ++i)
-			{
-				AddTask.call();
-			}
-		}
-
-		for(var i = 0; i < previousList.length; ++i)
-		{
-			taskString = previousList[previousList.length - 1 - i].split(":");
-
-			task = taskString[0];
-			completed = taskString[1];
-
-			spans[i].nextElementSibling.value = task;
-			SaveTextInput.call(spans[i].nextElementSibling);
-
-			if(parseInt(completed, 10) === 1)
-			{
-				CrossOffTask.call(cb[i]);
-			}
-			
-		}
-	}
-	else
-	{
-		console.log("nothing to load");
-	}
-}
